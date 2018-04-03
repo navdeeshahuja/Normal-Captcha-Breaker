326c88337030e81e010d3e92eae934bf3fdcb3e8
Navdeesh Ahuja <navdeeshahuja@Navdeeshs-MacBook-Air.local>
2018-04-03 15:34:45 +0530

    accuracy added

--- walker.py
+++ walker.py
@@ -12,11 +12,13 @@ count=1
 maxtime=0
 mintime=100
 currtime=0
+correctCount = -1
 for im in f:
 	img=Image.open("samples/"+str(im))
 	c=CaptchaParser()
 	starttime = timeit.default_timer()
-	print "CAPTCHA:"+c.getCaptcha(img)
+	capcthaText = c.getCaptcha(img)
+	print "CAPTCHA: "+capcthaText
 	endtime = timeit.default_timer()
 	currtime=endtime-starttime
 	if(currtime>maxtime):
@@ -30,12 +32,17 @@ for im in f:
 
 	timesum+=currtime
 	print "Comparing image "+str(count)+"/"+str(size)
+	originalCaptcha = (im.split(".")[0]).split("-")[1]
+	if(originalCaptcha == capcthaText):
+		correctCount += 1
 	count+=1
 avg=timesum/size
+accuracy = (float(correctCount) / float(size))*100.0
 
 print "========================================"
 print "Maximum Time:"+str(maxtime)
 print "Minimum Time:"+str(mintime)
 print "Total Time:"+str(timesum)
 print "Average Time:"+str(avg)
+print "Accuracy:"+str(accuracy)+" %"
 print "========================================"
